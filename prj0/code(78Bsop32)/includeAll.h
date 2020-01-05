9b9840da955db836574da0b2df677c36d9983da7
2017115241TenxMcu <1145545868@qq.com>
2020-01-05 21:17:39 +0800

    增加探温头

--- prj0/code(78Bsop32)/includeAll.h
+++ prj0/code(78Bsop32)/includeAll.h
@@ -1,9 +1,11 @@
 #include <REGtenxTM52F5278B.h>
 
- #include "typeAlias.h"
+#include "typeAlias.h"
 
- #include "display.h"
- #include "init.h"
- #include "main.h"
+#include "display.h"
+#include "init.h"
+#include "isr.h"
+#include "keys.h"
+#include "main.h"
 
- #include <INTRINS.H>
+#include <INTRINS.H>
